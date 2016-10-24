19a8d1fa98d316566f5dcdc9414caa1d5120ae44
AndreAndre <andregit91@gmail.com>
2016-10-24 20:27:13 +0600

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -2,6 +2,9 @@ package pack1;
 
 /**ghjjhg
  * jhgj
+ вапвапвап
+ вапвапв
+ вапвап
  * Created by Andre on 24.10.2016.
  */
 public class Solution {
@@ -9,7 +12,7 @@ public class Solution {
     public static void main(String[] args){
         int a = 5;
         int b = 7;
-
+ 
 
 
 
