ee664720833c526bdb8475c9a1922fe0a8ff748f
AndreAndre <andregit91@gmail.com>
2016-10-24 18:47:38 +0600

    ва

---
+++ Solution.java
@@ -0,0 +1,18 @@
+package pack1;
+
+/**
+ * Created by Andre on 24.10.2016.
+ */
+public class Solution {
+
+    public static void main(String[] args){
+        int a = 5;
+        int b = 7;
+
+        System.out.println(a + b);
+
+        System.out.println("suk");
+    }
+
+}
+
